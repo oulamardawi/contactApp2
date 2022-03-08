08e04d61aa96c9acd02062936664b7ef197b0171
Mardawi <ola.mardawi@gmail.com>
2022-03-08 23:49:44 +0200

    Initial Commit

---
+++ contactApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contactApp2
+//
+//  Created by Oula mardawi on 08/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
